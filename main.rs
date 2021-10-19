4e4f97080e4eee2350bff7ccd3fcff3ef32dae20
Rajkumar Ramakrishnan <rajkumar.rkrish@gmail.com>
2021-10-19 23:49:46 +0530

    Implemented in Rust

--- main.rs
+++ main.rs
@@ -1,1 +1,26 @@
-//Todo: Implement it in Rust
+fn main(){
+    const ITERATIONS:i32 = 100000000;
+    const ERROR_RATE:f64 = 0.0005;
+
+    let input:f64 = 7588888654.0;
+
+    let seed = input;
+    let mut solution = seed;
+    let mut error:f64 = -1.0; 
+
+    if input < 0.0 {
+        println!("Cannot compute square roots for negative numbers");
+        return;
+    }
+
+    for i in 0..ITERATIONS {
+        error = f64::powi(solution,2) - input;
+        if error < ERROR_RATE && error > -ERROR_RATE {
+            println!("Solution Reached on iteration: {0}", i);
+            break;
+        }
+        solution -= error/input;
+    }
+
+    println!("Solution: {0}\tError: {1}", solution, error);
+}
